2d9bb99bacf1fccc391ca4b9d6039e48ae783516
Shaswata Das <shaswata0999@gmail.com>
2020-11-07 18:28:45 +0530

    Update FindKeyword.py

--- FindKeyword.py
+++ FindKeyword.py
@@ -1,3 +1,20 @@
+'''
+One programming language has the following keywords that cannot be used as identifiers:
+
+break, case, continue, default, defer, else, for, func, goto, if, map, range, return, struct, type, var
+
+Write a program to find if the given word is a keyword or not
+Test cases
+Case 1
+
+    Input – defer
+    Expected Output – defer is a keyword
+
+Case 2
+
+    Input – While
+    Expected Output – while is not a keyword
+'''
 key = ['break','case','continue','default','defer','else','for','func','goto','if','map','range','return','struct','type','var']
 inp = input()
 if inp in key:
